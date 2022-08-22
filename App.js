b5ceb04ea3ac9179588cac44ca883c788cde3b62
ldbolk <ldbolk@ziggo.nl>
2022-08-22 11:29:12 +0200

    Started BluesShack exercise

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, SafeAreaView, Image } from 'react-native';
+import * as Styles from './app/resources/styles/Styles'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const img = "https://picsum.photos/id/1025/4951/3301"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      isError: false,
+      data: {}
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    this.setState({
+      isLoaded: true,
+      isError: false,
+      data: {}
+    })
+  }
+
+  renderContent() {
+    if(this.state.isLoaded) {
+      return(
+        <View>
+          <Text style={ Styles.regularText }>
+            The content of the page
+          <Image source={{uri: img}} style={{height:300, width: 300}}/>
+          </Text>
+        </View>
+      )
+    }
+
+    if(this.state.isError) {
+      return(
+        <View>
+          <Text>
+            An error happened
+          </Text>
+        </View>
+      )
+    }
+
+    return(
+    <View>
+      <Text>
+        Loading...
+      </Text>
+    </View>)
+  }
+
+  render() {
+    return(
+      <SafeAreaView style={ Styles.page }>
+        { this.renderContent() }
+      </SafeAreaView>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ app/resources/styles/Styles.js
@@ -0,0 +1,16 @@
+const page = {
+    backgroundColor: '#000',
+    flex: 1
+}
+
+const regularText = {
+    marginTop: 100,
+    fontsize: '16px',
+    padding: 20,
+    color: 'white'
+}
+
+export {
+    page,
+    regularText
+}
