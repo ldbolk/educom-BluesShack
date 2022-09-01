b71f40cb24af7e694309ea3904ad0c0de01d0da7
ldbolk <ldbolk@ziggo.nl>
2022-09-01 16:40:45 +0200

    Revert "Removed testcollection snapshot"
    This reverts commit bfbbb81139dd74997ad490882068eb4b4db0cc41.

--- App.js
+++ App.js
@@ -133,6 +133,16 @@ class App extends Component {
       // })
       
       
+      
+      const testDoc = doc(db, '/Artist/1') // doc instead of document because document is used in other languages I think?
+      
+      onSnapshot(testDoc, (snapshot) => { // This works!
+        let test = []
+        test.push(snapshot.data())
+        console.log('wee -----------------', snapshot.data())
+      })
+      
+      
       // Snapshot can be used to catch specific a user based on userId
       
 
@@ -163,6 +173,7 @@ class App extends Component {
         snapshot.docs.forEach((doc) => {
           user.push({ ...doc.data(), id: doc.id })
         })
+        console.log('User: ', user)
         
         user[0].Artists.forEach(e => {
           favoriteDocuments.push(doc(db, e.path))
@@ -252,6 +263,7 @@ class App extends Component {
 
   renderContent() {
     if(this.state.isLoaded) {
+      console.log('oh there was one here ', this.state.favorites)
       return(
         <NavigationContainer>
           <Stack.Navigator screenOptions={{headerShown: false}}>
